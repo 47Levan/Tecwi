cc3ed3cb2685350811f5ac78edefa30b5e89382b
47Levan <47Vthctltc>
2016-10-07 14:49:24 +0300

    Database added

---
+++ BookShop/BookShop/Models/BooksDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using BookShop.Models.Entities;
+
+namespace BookShop.Models
+{
+    public class BooksDbContext : DbContext
+    {
+        public BooksDbContext():base("BooksConnection")
+        {
+            
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
